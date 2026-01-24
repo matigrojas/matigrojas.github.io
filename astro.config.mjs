14ce79a4874d198d1d5bf2f8a5073c5b68f53c32
matigrojas <rojasmatias994@gmail.com>
2026-01-24 16:23:04 -0300

    i18n multi-language release

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // URL principal de tu sitio
   site: 'https://matigrojas.github.io',
-  
-  // Si el repo se llama 'matigrojas.github.io', base debe ser '/' o no estar.
-  // Solo usarías base: '/nombre-repo' si el repositorio tuviera otro nombre.
-  base: '/', 
+  base: '/',
+  // Si añadiste redirecciones para el multi-idioma, van aquí
+  redirects: {
+    '/': '/es',
+  },
 });
